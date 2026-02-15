8c7a63e030da659eb67d689a1a1957b63aa4dfd0
Dr_John <efexzium@gmail.com>
2026-02-14 18:32:23 -0600

    Add Render deployment configuration

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 
-const String apiBaseUrl = kReleaseMode
-    ? 'https://fastapi-openrouter-api.onrender.com'
-    : 'http://localhost:8000';
+const String apiBaseUrl = String.fromEnvironment('API_BASE_URL',
+    defaultValue: kReleaseMode
+        ? 'https://fastapi-openrouter-api.onrender.com'
+        : 'http://localhost:8000');
