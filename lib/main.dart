b7be2f6832065d7a079db44f14632fdd287984bd
Dr_John <efexzium@gmail.com>
2026-02-23 10:08:37 -0600

    Log in flow

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,5 @@ import 'screens/chat_screen.dart';
 import 'theme/app_theme.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'AI Chat',
-      theme: appTheme(),
-      home: const ChatScreen(),
-    );
-  }
+  runApp(const AuthWrapper());
 }
